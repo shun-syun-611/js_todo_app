734c6c59ee97d9bbc1f42ef973bd67a2ce582493
Shun Sugawara <82685781shun-syun-611@users.noreply.github.com>
2021-12-28 10:47:31 +0900

    add: 入力したtodoタスクをリロードすると内容が消えるため、ローカルストレージで保存、取得を実装

--- todo.js
+++ todo.js
@@ -3,6 +3,37 @@
     const taskSubmit = document.getElementsByClassName('task_submit')[0];
     const taskList = document.getElementsByClassName('task_list')[0];
 
+    // ローカルストレージの初期化
+    let listItems = [];
+    const storage = localStorage;
+
+    // リロードされた時に、ローカルストレージの保存内容を表示
+    document.addEventListener("DOMContentLoaded", () => {
+        const json = storage.store;
+        if (json === undefined) {
+            return;
+        }
+        listItems = JSON.parse(json);
+        for (const item of listItems) {
+            const stock_task = document.createTextNode(item.todoValue);
+            const listItem = document.createElement("li");
+            const showItem = taskList.appendChild(listItem);
+            const showTask = showItem.appendChild(stock_task);
+
+            // 以下、二つの処理は後で細かく共通化できそう
+            // 追加したタスクに削除ボタンを付与
+            const deleteButton = document.createElement("button");
+            deleteButton.innerHTML = "Delete";
+            listItem.appendChild(deleteButton);
+
+            // 削除ボタンをクリックしたら、削除イベント発火
+            deleteButton.addEventListener("click", e => {
+                e.preventDefault();
+                deleteTasks(deleteButton);
+            });
+            }
+    });
+
     // 追加ボタンを作成
     const addTasks = (task) => {
         // 入力したタスクの追加
@@ -32,6 +63,18 @@
     taskSubmit.addEventListener('click', e => {
         e.preventDefault();
         const task = taskValue.value;
-        addTasks(task);
-        taskValue.value = '';
+        if (task !== ""){
+            // ローカルストレージに保存
+            const item = {
+                todoValue: task,
+                isDeleted: false
+            };
+            listItems.push(item);
+            storage.store = JSON.stringify(listItems);
+
+            addTasks(task);
+            taskValue.value = '';
+        }
     });
+
+    
